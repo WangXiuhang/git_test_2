47956f21bd634afa4002413976773c617cf4df75
Leo <2042878370@qq.com>
2022-02-19 17:46:49 +0800

    first commit

---
+++ src/main/java/cn/edu/zju/GitTest.java
@@ -0,0 +1,13 @@
+package cn.edu.zju;
+
+/**
+ * @Author WangXH
+ * @Date 2022/2/19 17:43
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
